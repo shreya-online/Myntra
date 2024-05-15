2ed651690c98dc7703ce74e1cb6274f871b80e9e
Shreya Manna <shreya.kolkata07@gmail.com>
2024-05-16 03:06:39 +0530

    Initial changes in login page

--- myntra-ui-store/src/components/LoginOrSignup.js
+++ myntra-ui-store/src/components/LoginOrSignup.js
@@ -1,15 +1,51 @@
 import React from 'react';
+import Grid from '@mui/material/Grid';
+import Card from '@mui/material/Card';
+import CardMedia from '@mui/material/CardMedia';
+import Typography from '@mui/material/Typography';
+import TextField from '@mui/material/TextField';
+import InputAdornment from '@mui/material/InputAdornment';
+import OutlinedInput from '@mui/material/OutlinedInput';
+import FormControl from '@mui/material/FormControl';
 import loginImage from '../assets/img/loginBanner.jpeg'
 
 function loginOrSignup(){
-    return(
-        <>
-            <img src={loginImage} alt='loginImage'/>
-            <div>
-                <h3>Login or Signup</h3>
-            </div>
-        </>
-    )
+    return (
+      <>
+        <Card style={{ backgroundColor: "pink" }}>
+          <Grid
+            container
+            justifyContent="center"
+            direction="column"
+            alignItems="center"
+            style={{ marginTop: "25px" }}
+          >
+            <Grid item xs={6}>
+              <CardMedia
+                component="img"
+                height="170"
+                src={loginImage}
+                alt="loginImage"
+              />
+              {/* <img height= '150px'src={loginImage} alt='loginImage'/> */}
+            </Grid>
+            <Grid item xs={6}>
+              <Typography gutterBottom> <strong>Login</strong> or <strong>Signup</strong></Typography>
+              <FormControl sx={{ m: 1, width: '25ch' }} variant="outlined">
+                <OutlinedInput
+                    // id="outlined-adornment-weight"
+                    startAdornment={<InputAdornment position="start">+91 | </InputAdornment>}
+                    aria-describedby="outlined-weight-helper-text"
+                    inputProps={{
+                    // 'aria-label': 'weight',
+                    }}
+                />
+              </FormControl>
+            </Grid>
+          </Grid>
+        </Card>
+      </>
+    );
 
 }
 
